b839e2bce4fbfbd30008fba82cda781a9f38bb97
liukun <352414257@qq.com>
2024-07-05 09:48:30 +0800

    1

--- src/Kernel/BaseRequest.php
+++ src/Kernel/BaseRequest.php
@@ -3,7 +3,6 @@
 namespace Liukangkun\Kuaishou\Kernel;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Utils;
 use Liukangkun\Kuaishou\Kernel\Exception\InvalidParamException;
 use Liukangkun\Kuaishou\Kernel\Exception\KuaishouException;
@@ -15,18 +14,13 @@ class BaseRequest
 {
     protected $httpClient;
     protected $baseUri = 'https://ad.e.kuaishou.com/rest/openapi/';
-    protected $defaults = [
-        'curl' => [
-            CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
-        ],
-    ];
+
 
     public function getHttpClient()
     {
         if (!$this->httpClient) {
             $this->httpClient = new Client([
-                'base_uri' => $this->baseUri,
-                'handler' => HandlerStack::create($this->getGuzzleHandler())
+                'base_uri' => $this->baseUri
             ]);
         }
         return $this->httpClient;
@@ -37,8 +31,9 @@ class BaseRequest
      * @param string $method
      * @param array $options
      * @return array|bool|float|int|object|string|null
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws InvalidParamException
      * @throws KuaishouException
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function httpRequest(string $url, string $method = 'GET', array $options = [])
     {
@@ -47,20 +42,12 @@ class BaseRequest
         if (!in_array($method, $validMethods, true)) {
             throw new InvalidParamException("Invalid HTTP method: {$method}");
         }
-        $options = array_merge($this->defaults, $options);
         try {
             $response = $this->getHttpClient()->request($method, $url, $options);
-
-            // 4. 优化响应处理
             $body = (string)$response->getBody();
         } catch (\Exception $e) {
             throw new KuaishouException("Failed to perform HTTP request to {$url}", 501);
         }
         return Utils::jsonDecode($body, true);
-    }
-
-    protected function getGuzzleHandler()
-    {
-        return Utils::chooseHandler();
     }
 }
