a315ed4a52bd333e25bdf9fcfcf8809f7e7892ed
liukun <352414257@qq.com>
2024-07-12 17:59:39 +0800

    新增支持远端

--- src/Tool/File/Client.php
+++ src/Tool/File/Client.php
@@ -90,13 +90,14 @@ class Client extends BaseClient
     /**
      * 文件流方式上传
      * @param string $file 本地文件路径
+     * @param string $file_name 文件名称
      * @param string $file_type 文件类型，视频文件传 "mp4"
      * @return array|bool|float|int|object|string|null
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \Liukangkun\Kuaishou\Kernel\Exception\InvalidParamException
      * @throws \Liukangkun\Kuaishou\Kernel\Exception\KuaishouException
      */
-    public function streamUploadVideo(string $file, string $file_type = 'mp4')
+    public function streamUploadVideo(string $file, string $file_name = '', string $file_type = 'mp4')
     {
         $token = $this->getUploadToken($file_type);
         if ($token['code']) {
@@ -123,12 +124,13 @@ class Client extends BaseClient
 
     /**
      * 分片上传视频
-     * @param string $file
+     * @param string $file 文件地址
+     * @param string $file_name 文件名称
      * @param string $file_type
      * @return array|bool|float|int|object|string|null
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function chunkUploadVideo(string $file, string $file_type = 'mp4')
+    public function chunkUploadVideo(string $file, string $file_name = '', string $file_type = 'mp4')
     {
         //检测文件后缀是否为mp4
 //        if (pathinfo($file, PATHINFO_EXTENSION) != 'mp4') {
@@ -155,13 +157,112 @@ class Client extends BaseClient
         }
         //获取blob_store_key
         $blob_store_key = $verify_res['data']['blob_store_key'];
-        //获取文件名称和文件后缀
-        $file_name = pathinfo($file, PATHINFO_FILENAME);
+        //获取文件名称
+//        $file_name = pathinfo($file, PATHINFO_FILENAME);
         //上传到快手视频库
         return $this->uploadVideo(['blob_store_key' => $blob_store_key, 'photo_name' => $file_name, 'signature' => md5_file($file)]);
     }
 
+    /**
+     * 兼容远程文件上传
+     * @param $file
+     * @param $endpoint
+     * @param $upload_token
+     * @param $chunk_size
+     * @return false|float
+     */
     private function chunkUpload($file, $endpoint, $upload_token, $chunk_size = 1024 * 1024 * 5)
+    {
+        // 检查文件是否为本地路径或URL
+        if (filter_var($file, FILTER_VALIDATE_URL)) {
+            // 远程文件处理
+            return $this->chunkUploadRemote($file, $endpoint, $upload_token, $chunk_size);
+        } else {
+            // 本地文件处理
+            return $this->chunkUploadLocal($file, $endpoint, $upload_token, $chunk_size);
+        }
+    }
+
+    private function chunkUploadLocal($file, $endpoint, $upload_token, $chunk_size)
+    {
+        // 检查文件是否存在和大小
+        if (!file_exists($file) || !is_readable($file)) {
+            throw new \RuntimeException("File does not exist or is not readable.");
+        }
+
+        $file_size = filesize($file);
+        //限制分片文件大小
+//        if ($file_size < $chunk_size) {
+//            throw new \InvalidArgumentException("File size is smaller than the chunk size.");
+//        }
+
+        $f = @fopen($file, 'rb');
+        if (!$f) {
+            throw new \RuntimeException("Failed to open file.");
+        }
+
+        $promises = [];
+        $chunk_count = ceil($file_size / $chunk_size);
+        for ($i = 0; $i < $chunk_count; $i++) {
+            $start = $i * $chunk_size;
+            fseek($f, $start);
+            $buffer = fread($f, $chunk_size);
+            if ($buffer === false) {
+                throw new \RuntimeException("Failed to read from file.");
+            }
+            $promises[] = $this->fragmentUpload($buffer, $i, $endpoint, $upload_token);
+        }
+
+        fclose($f);
+        Promise\Utils::unwrap($promises);
+
+        return $chunk_count;
+    }
+
+    private function chunkUploadRemote($file, $endpoint, $upload_token, $chunk_size)
+    {
+        $client = $this->getHttpClient();
+
+        // 验证URL
+        if (!filter_var($file, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException("Invalid file URL.");
+        }
+
+        // 获取远程文件的大小
+        $response = $client->request('HEAD', $file);
+        $file_size = intval($response->getHeaderLine('Content-Length'));
+        //限制分片文件大小
+//        if ($file_size < $chunk_size) {
+//            throw new \InvalidArgumentException("File size is smaller than the chunk size.");
+//        }
+
+        $promises = [];
+        $chunk_count = ceil($file_size / $chunk_size);
+
+        for ($i = 0; $i < $chunk_count; $i++) {
+            $start = $i * $chunk_size;
+            $end = min($start + $chunk_size - 1, $file_size - 1);
+
+            $options = [
+                'headers' => [
+                    'Range' => "bytes=$start-$end",
+                ],
+            ];
+
+            $promises[] = $client->getAsync($file, $options)->then(function ($response) use ($i, $endpoint, $upload_token) {
+                $buffer = $response->getBody()->getContents();
+                return $this->fragmentUpload($buffer, $i, $endpoint, $upload_token);
+            });
+        }
+
+        $promise = Promise\Utils::all($promises);
+        $promise->wait();
+
+        return $chunk_count;
+    }
+
+
+    private function chunkUploadBak($file, $endpoint, $upload_token, $chunk_size = 1024 * 1024 * 5)
     {
         //检测文件后缀
         //$size = filesize($file);
